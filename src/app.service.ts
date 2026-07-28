4899855290f9ab2b283553472d11452387b651ac
wizard113 <yeong.rok.lee@gmail.com>
2026-07-28 14:19:10 +0900

    6th publish

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!-app service 단독 배포';
+  getHello() {
+    const dbUrl = process.env.DATABASE_URL ?? '(미설정)';
+    const conStr = process.env.BLOB_CONNECTION_STRING ?? '(미설정)';
+
+    return {
+      message: 'Hello World!-app service',
+      DATABASE_URL: dbUrl,
+      BLOB_CONNECTION_STRING: conStr,
+    };
   }
 }
